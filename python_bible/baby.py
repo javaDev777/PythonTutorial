e7f207f756eb8c46ec88317061e71e01803e75f4
javaDev777 <Lolita.young777@gmail.com>
2022-07-12 16:11:20 -0400

    Second commit

--- python_bible/baby.py
+++ python_bible/baby.py
@@ -1,6 +1,6 @@
 from random import choice
 
-questions = ["Why is the sky blue?: ", "Why is there a face on the moon?: ",
+questions = ["Why is the sky purple?: ", "Why is there a face on the moon?: ",
                      "Where are all the dinosaurs: "]
 
 question = choice(questions)
